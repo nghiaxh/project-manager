9651d9f08ad1b5e2eefb3609dd09f89ca9bc31be
Nghia Hoang <nghiahoang880@gmail.com>
2026-02-26 11:16:25 +0700

    fix: remove exclude database class

--- server/src/main/java/com/pm/project_manager/ProjectManagerApplication.java
+++ server/src/main/java/com/pm/project_manager/ProjectManagerApplication.java
@@ -2,9 +2,8 @@ package com.pm.project_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class ProjectManagerApplication {
 
 	public static void main(String[] args) {
