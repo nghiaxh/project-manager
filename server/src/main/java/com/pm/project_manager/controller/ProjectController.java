8c7e05627427efb894b52010d4098f5126d8e235
damvinhhhung205 <damvinhhung9a2@gmail.com>
2026-03-05 11:25:59 +0700

    feat: update controllers

--- server/src/main/java/com/pm/project_manager/controller/ProjectController.java
+++ server/src/main/java/com/pm/project_manager/controller/ProjectController.java
@@ -2,9 +2,10 @@ package com.pm.project_manager.controller;
 
 import com.pm.project_manager.dto.ProjectDto;
 import com.pm.project_manager.dto.ProjectMemberDto;
-import com.pm.project_manager.model.ProjectRole;
 import com.pm.project_manager.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,13 +17,16 @@ public class ProjectController {
     private final ProjectService projectService;
 
     @GetMapping
-    public List<ProjectDto> getUserProjects(@RequestParam Long userId) {
-        return projectService.getProjectsByUser(userId);
+    public List<ProjectDto> getUserProjects(@AuthenticationPrincipal UserDetails currentUser) {
+        String username = currentUser.getUsername();
+        return projectService.getProjectsByUser(username);
     }
 
     @PostMapping
-    public ProjectDto createProject(@RequestBody ProjectDto dto, @RequestParam Long createdBy) {
-        return projectService.createProject(dto, createdBy);
+    public ProjectDto createProject(@RequestBody ProjectDto dto,
+            @AuthenticationPrincipal UserDetails currentUser) {
+        String username = currentUser.getUsername();
+        return projectService.createProject(dto, username);
     }
 
     @GetMapping("/{id}")
@@ -41,8 +45,9 @@ public class ProjectController {
     }
 
     @PostMapping("/{projectId}/members")
-    public void addMember(@PathVariable Long projectId, @RequestParam Long userId, @RequestParam ProjectRole role) {
-        projectService.addMember(projectId, userId, role);
+    public void addMember(@PathVariable Long projectId,
+            @RequestParam Long userId) {
+        projectService.addMember(projectId, userId);
     }
 
     @GetMapping("/{projectId}/members")
