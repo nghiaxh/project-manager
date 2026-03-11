75c8d855adb10fca440f28240a4b100a962687a0
Nghia Hoang <nghiahoang880@gmail.com>
2026-03-11 21:43:11 +0700

    fix: use user id

--- server/src/main/java/com/pm/project_manager/repository/ProjectMemberRepository.java
+++ server/src/main/java/com/pm/project_manager/repository/ProjectMemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.pm.project_manager.model.ProjectMember;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
@@ -11,5 +12,7 @@ public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Lo
 
     List<ProjectMember> findByUserId(Long userId);
 
-    boolean existsByProjectIdAndUserUsername(Long projectId, String username);
+    boolean existsByProjectIdAndUserId(Long projectId, Long userId);
+    
+    Optional<ProjectMember> findByProjectIdAndUserId(Long projectId, Long userId);
 }
