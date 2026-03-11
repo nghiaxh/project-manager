d10929619ceb476cc9fdccbd7760980b1dfdb0c9
Nghia Hoang <nghiahoang880@gmail.com>
2026-03-11 21:48:02 +0700

    chore: main view config, add notivue

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./index.css";
+import { createApp } from 'vue';
+import { createNotivue } from "notivue";
+import App from './App.vue';
+import router from './router';
 
-createApp(App).mount("#app");
+import './index.css';
+import 'notivue/notification.css';
+import 'notivue/animations.css';
+
+const app = createApp(App);
+const notivue = createNotivue({
+    position: 'top-right',
+    limit: 5,
+    enqueue: true,
+
+});
+app.use(router);
+app.use(notivue);
+
+app.mount('#app');
