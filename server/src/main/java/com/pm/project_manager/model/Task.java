3184ee579c06667e25e7f57a954074138b7dfbf0
Nghia Hoang <nghiahoang880@gmail.com>
2026-03-15 21:09:04 +0700

    refactor: replace status, add text field

--- server/src/main/java/com/pm/project_manager/model/Task.java
+++ server/src/main/java/com/pm/project_manager/model/Task.java
@@ -20,6 +20,8 @@ public class Task {
     private Long id;
 
     private String title;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
 
     @Enumerated(EnumType.STRING)
--- server/src/main/java/com/pm/project_manager/model/TaskStatus.java
+++ server/src/main/java/com/pm/project_manager/model/TaskStatus.java
@@ -3,6 +3,6 @@ package com.pm.project_manager.model;
 public enum TaskStatus {
     TODO,
     IN_PROGRESS,
+    IN_REVIEW,
     DONE,
-    CANCELLED
 }
