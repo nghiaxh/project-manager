8f3a76fc01ade3ca08bec4d61e47fc97c2d51ba1
HungB2303818 <hungb2303818@student.ctu.edu.vn>
2026-03-19 10:17:40 +0700

    fix: gitignore & UI update

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./index.css";
+import { createApp } from 'vue';
+import { createNotivue } from "notivue";
+import App from './App.vue';
+import router from './router';
 
-createApp(App).mount("#app");
+import './index.css';
+import 'notivue/notification.css';
+import 'notivue/animations.css';
+import 'notivue/notification-progress.css';
+
+const app = createApp(App);
+const notivue = createNotivue({
+    position: 'top-center',
+    limit: 2,
+    enqueue: true,
+
+});
+app.use(notivue);
+app.use(router);
+
+app.mount('#app');
