450a270546af514a3a8bf08ec969272aebc8d3bd
HungB2303818 <hungb2303818@student.ctu.edu.vn>
2026-03-24 10:20:17 +0700

    test: intergration test for notificationController (PASSED)

--- server/src/test/java/com/pm/project_manager/repository/ProjectMemberRepositoryTest.java
+++ server/src/test/java/com/pm/project_manager/repository/ProjectMemberRepositoryTest.java
@@ -7,11 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
-
+@ActiveProfiles("test")
 class ProjectMemberRepositoryTest {
 
     @Autowired
